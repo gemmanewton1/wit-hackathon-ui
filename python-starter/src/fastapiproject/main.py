a1f2bd0c1b6d03d4c2daf18d216c7f0772b03f53
James.Kempadoo <james.kempadoo@paconsulting.com>
2025-09-01 10:32:18 +0100

    feat: add CORS middleware and configuration to FastAPI app

--- python-starter/src/fastapiproject/main.py
+++ python-starter/src/fastapiproject/main.py
@@ -2,9 +2,25 @@
 from fastapi import FastAPI
 from .routers.customer_router import router as customer_router  # Import the customer router
 from .routers.product_router import router as product_router  # Import the product router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Create a FastAPI app instance
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include the customer and product routers
 app.include_router(customer_router)
