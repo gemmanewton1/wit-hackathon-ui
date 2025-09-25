f6643c835ee409a56602995fd0d83ebabedd5caf
chloe.price <chloe.price@paconsulting.com>
2025-09-25 14:37:44 +0100

    landing

--- react-starter/src/Components/LandingPage.jsx
+++ react-starter/src/Components/LandingPage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import StreakSection from "./Streak";
-import SpeechBubble from "./SpeechBubble";
 import SheKnowsBot from "./Bot";
 
 function LandingPage() {
