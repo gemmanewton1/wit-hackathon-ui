566b4be2ececd5ada2368ca660de7c1ccae707dd
iimaanm <iimaan.mohamed@paconsulting.com>
2025-09-25 14:28:46 +0100

    navbar and background

--- react-starter/src/Components/LandingPage.jsx
+++ react-starter/src/Components/LandingPage.jsx
@@ -4,20 +4,6 @@ import { Link } from "react-router-dom";
 function LandingPage() {
   return (
     <div className="landing-container">
-      <nav>
-        <div className="logo">MyApp</div>
-        <ul>
-          <li>
-            <Link to="/">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/communities">Communities</Link>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
       <section className="hero">
         <h1>Welcome to MyApp</h1>
         <p>Your new favorite app for everything.</p>
