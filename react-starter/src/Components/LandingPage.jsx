d86b2ac7134b68988fa254fd12c467a74f103174
chloe.price <chloe.price@paconsulting.com>
2025-09-25 14:42:06 +0100

    styling

--- react-starter/src/Components/LandingPage.jsx
+++ react-starter/src/Components/LandingPage.jsx
@@ -5,26 +5,7 @@ import SheKnowsBot from "./Bot";
 function LandingPage() {
   return (
     <div className="landing-container">
-      <nav>
-        <div className="logo">MyApp</div>
-        <ul>
-          <li>
-            <Link to="/">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/communities">Communities</Link>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
       <div>
-        <button>Export</button>
-        {/* </div>
-      <div className="flex justify-center items-center h-screen bg-gray-100"> */}
-        {/* <SpeechBubble text="Hello! I'm a speech bubble." /> */}
-
         <SheKnowsBot />
       </div>
     </div>
