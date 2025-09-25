413b5722e389885437ce16ca3d16c71b5d2a7124
Michelle Yip <michelle.yip@paconsulting.com>
2025-09-25 12:47:43 +0100

    Update project to maven

--- java-starter/src/main/java/com/example/demo/HackathonStarterApplication.java
+++ java-starter/src/main/java/com/example/demo/HackathonStarterApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.Utils.SSLBypass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HackathonStarterApplication {
 
 	public static void main(String[] args) {
+        SSLBypass.disableSSLVerification();
 		SpringApplication.run(HackathonStarterApplication.class, args);
-
 	}
 
 }
